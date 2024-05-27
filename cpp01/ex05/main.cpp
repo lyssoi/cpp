3e36ffbaac12740be4b12474c27fcfeb7a24dcc6
lyssoi <114637463+lyssoi@users.noreply.github.com>
2024-05-27 17:49:05 +0900

    cpp01

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main()
+{
+    Harl Harl;
+
+    Harl.complain("debug");
+    Harl.complain("info");
+    Harl.complain("warning");
+    Harl.complain("error");
+    return (0);
+}
