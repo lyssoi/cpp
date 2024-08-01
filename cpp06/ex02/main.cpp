db5abe4903d7cf6d224816456fc94e8f745394c7
Sol Jeong <soljeong@c2r4s7.42seoul.kr>
2024-08-01 16:17:19 +0900

    feat

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -30,22 +30,35 @@ void identify(Base *p) {
 }
 
 void identify(Base &p) {
-    if (dynamic_cast<A *>(&p)){
+    try {
+        A &a = dynamic_cast<A &>(p);
         std::cout << "Reference type A " << std::endl;
-    } else if (dynamic_cast<B *>(&p)) {
+        static_cast<void>(a);
+    } catch(std::exception &e) {};
+    try {
+        B &b = dynamic_cast<B &>(p);
         std::cout << "Reference type B " << std::endl;
-    } else if (dynamic_cast <C *>(&p)) {
+        static_cast<void>(b);
+    } catch(std::exception &e) {};
+    try {
+        C &c = dynamic_cast<C &>(p);
         std::cout << "Reference type C " << std::endl;
-    }
+        static_cast<void>(c);
+    } catch(std::exception &e) {};
 }
 
 int main() {
 
     Base *base;
+    A a;
+    B b;
+    C c;
 
     base = generate();
     identify(base);
-    identify(*base);
+    identify(a);
+    identify(b);
+    identify(c);
 
     delete base;
 }
